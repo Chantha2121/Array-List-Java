daff7b193afed471606b624d950b27bf708f69fd
Eventlistener <chanfcsg334@gmail.com>
2024-11-11 16:20:10 +0700

    Array List 1

---
+++ src/ArrList.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class ArrList {
+    public static void main(String[] args) {
+        ArrayList<String> fruits = new ArrayList<>();
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Coconut");
+        System.out.println(fruits);
+    }
+}
